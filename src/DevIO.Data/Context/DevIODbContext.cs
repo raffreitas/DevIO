142b3f0565db929d3c6bccbed26d59198efbd73d
Rafael Freitas <raffreitasx@outlook.com>
2024-04-21 09:58:08 -0300

    feat: add custom config EF

--- src/DevIO.Data/Context/DevIODbContext.cs
+++ src/DevIO.Data/Context/DevIODbContext.cs
@@ -6,6 +6,8 @@ public class DevIODbContext : DbContext
 {
     public DevIODbContext(DbContextOptions<DevIODbContext> options) : base(options)
     {
+        ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
+        ChangeTracker.AutoDetectChangesEnabled = false;
     }
 
     public DbSet<Product> Products { get; set; }
@@ -23,5 +25,23 @@ public class DevIODbContext : DbContext
             relationship.DeleteBehavior = DeleteBehavior.ClientSetNull;
 
         base.OnModelCreating(modelBuilder);
+    }
+
+    public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+    {
+        foreach (var entry in ChangeTracker.Entries().Where(entry => entry.Entity.GetType().GetProperty("CreatedAt") != null))
+        {
+            if (entry.State == EntityState.Added)
+            {
+                entry.Property("CreatedAt").CurrentValue = DateTime.Now;
+            }
+
+            if (entry.State == EntityState.Modified)
+            {
+                entry.Property("CreatedAt").IsModified = false;
+            }
+        }
+
+        return base.SaveChangesAsync(cancellationToken);
     }
 }
