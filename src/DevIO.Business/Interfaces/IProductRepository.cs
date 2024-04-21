175a7b7b85981547583e4afba72da8c8a2632c05
Rafael Freitas <raffreitasx@outlook.com>
2024-04-21 08:28:39 -0300

    feat: add interfaces

---
+++ src/DevIO.Business/Interfaces/IProductRepository.cs
@@ -0,0 +1,10 @@
+﻿using DevIO.Business.Models;
+
+namespace DevIO.Business.Interfaces;
+
+public interface IProductRepository : IRepository<Product>
+{
+    Task<IEnumerable<Product>> GetProductsBySupplier(Guid supplierId);
+    Task<IEnumerable<Product>> GetProductsSuppliers();
+    Task<Product> GetProductSupplier(Guid id);
+}
