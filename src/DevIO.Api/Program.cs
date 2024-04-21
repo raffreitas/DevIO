a9045f1b7b237a976baf0a106053f93f11334702
Rafael Freitas <raffreitasx@outlook.com>
2024-04-21 14:19:33 -0300

    feat: remove model state default validation filter

--- src/DevIO.Api/Program.cs
+++ src/DevIO.Api/Program.cs
@@ -4,7 +4,11 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .ConfigureApiBehaviorOptions(options =>
+    {
+        options.SuppressModelStateInvalidFilter = true;
+    });
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
